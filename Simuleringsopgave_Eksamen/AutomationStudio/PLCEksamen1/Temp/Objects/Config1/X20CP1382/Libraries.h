1d71fcfbb44dc32dbb1be443e4a4c4dda51044c3
Groed <Johan.hammer.jakobsen@gmail.com>
2020-05-11 15:12:42 +0200

    PLC Feeder kode ændring
    Ændring af feeder kode sådan at de først bliver sat lav igen 10 sek efter de er sat høj.

--- Simuleringsopgave_Eksamen/AutomationStudio/PLCEksamen1/Temp/Objects/Config1/X20CP1382/Libraries.h
+++ Simuleringsopgave_Eksamen/AutomationStudio/PLCEksamen1/Temp/Objects/Config1/X20CP1382/Libraries.h
@@ -1,8 +1,8 @@
 /* Automation Studio generated header file */
 /* Do not edit ! */
 
-#ifndef _LIBRARIES_20200511144930_
-#define _LIBRARIES_20200511144930_
+#ifndef _LIBRARIES_20200511150838_
+#define _LIBRARIES_20200511150838_
 
 __asm__(".section \".plc\"");
 
@@ -490,5 +490,5 @@ __asm__(".ascii \"iecfile \\\"Logical/Libraries/standard/standard.var\\\" scope 
 __asm__(".previous");
 
 
-#endif /* _LIBRARIES_20200511144930_ */
+#endif /* _LIBRARIES_20200511150838_ */
 
