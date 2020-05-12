2b0dbb297e6b94fc9eb675c9f7fc85ca0855962f
Groed <Johan.hammer.jakobsen@gmail.com>
2020-05-12 11:32:12 +0200

    ændret et variabel navn

--- Simuleringsopgave_Eksamen/AutomationStudio/PLCEksamen1/Temp/Objects/Config1/X20CP1382/Libraries.h
+++ Simuleringsopgave_Eksamen/AutomationStudio/PLCEksamen1/Temp/Objects/Config1/X20CP1382/Libraries.h
@@ -1,8 +1,8 @@
 /* Automation Studio generated header file */
 /* Do not edit ! */
 
-#ifndef _LIBRARIES_20200511203649_
-#define _LIBRARIES_20200511203649_
+#ifndef _LIBRARIES_20200512112919_
+#define _LIBRARIES_20200512112919_
 
 __asm__(".section \".plc\"");
 
@@ -490,5 +490,5 @@ __asm__(".ascii \"iecfile \\\"Logical/Libraries/standard/standard.var\\\" scope 
 __asm__(".previous");
 
 
-#endif /* _LIBRARIES_20200511203649_ */
+#endif /* _LIBRARIES_20200512112919_ */
 
