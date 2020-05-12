a9bce1dcb55c8b5593a36b9a392af254d45fc65c
Groed <Johan.hammer.jakobsen@gmail.com>
2020-05-12 15:57:16 +0200

    Timer ændringer

--- Simuleringsopgave_Eksamen/AutomationStudio/PLCEksamen1/Temp/Objects/Config1/X20CP1382/Libraries.h
+++ Simuleringsopgave_Eksamen/AutomationStudio/PLCEksamen1/Temp/Objects/Config1/X20CP1382/Libraries.h
@@ -1,8 +1,8 @@
 /* Automation Studio generated header file */
 /* Do not edit ! */
 
-#ifndef _LIBRARIES_20200512141528_
-#define _LIBRARIES_20200512141528_
+#ifndef _LIBRARIES_20200512154402_
+#define _LIBRARIES_20200512154402_
 
 __asm__(".section \".plc\"");
 
@@ -490,5 +490,5 @@ __asm__(".ascii \"iecfile \\\"Logical/Libraries/standard/standard.var\\\" scope 
 __asm__(".previous");
 
 
-#endif /* _LIBRARIES_20200512141528_ */
+#endif /* _LIBRARIES_20200512154402_ */
 
